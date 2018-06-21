f7cb48657cb11105c8f540d0eb1848c416c13c3f
Sytronik <sytronik210@gmail.com>
2018-06-22 01:16:25 +0900

    Convolved 데이터 저장

--- N2_signal_convolution.py
+++ N2_signal_convolution.py
@@ -1,11 +1,15 @@
 import pdb
+
+import numpy as np
 import scipy as sc
 import scipy.io as scio
 import scipy.io.wavfile as sciowav
 
 import librosa
 
-import numpy as np
+import os
+from glob import glob
+import soundfile as sf
 
 import pycuda.driver as cuda
 import pycuda.autoinit
@@ -15,16 +19,14 @@ from pycuda.compiler import SourceModule
 # __global__ void
 # """)
 
-import os
-from glob import glob
-
 RIR=np.array(scio.loadmat("./1_MATLABCode/RIR_Data/RIR.mat", variable_names="RIR")['RIR'])
 RIR=np.transpose(RIR,(2,0,1))
 # RIR.astype(np.float32)
 # RIR_gpu=cuda.mem_alloc(RIR.nbytes)
 # cuda.memcpy_htod(RIR_gpu, RIR)
 
-speech = []
+# speech = []
+# speech_convolved = []
 fs = 48000
 fs_original=0
 for dir,_,_ in os.walk("./speech/data/lisa/data/timit/raw/TIMIT/TRAIN/"):
@@ -38,20 +40,23 @@ for dir,_,_ in os.walk("./speech/data/lisa/data/timit/raw/TIMIT/TRAIN/"):
         else:
             data,_ = librosa.load(file)
         data = librosa.core.resample(data, fs_original, fs)
-        data=data.astype(np.float32)
+
+        # data = data.astype(np.float32)
         # speech.extend(data)
-        print(file)
         length = data.shape[0]
         N_loc = RIR.shape[0]
         N_ch = RIR.shape[1]
         # pdb.set_trace()
         for i_loc in range(0,N_loc):
-            convolved=np.zeros((N_ch, length))
+            convolved=np.zeros((length, N_ch))
             for i_ch in range(0,N_ch):
-                convolved[i_ch]=sc.signal.fftconvolve(data, RIR[i_loc, i_ch], mode='same')
+                convolved[:,i_ch]=sc.signal.fftconvolve(data, RIR[i_loc, i_ch], mode='same')
+            # convolved=np.asarray(convolved*,dtype=np.int16)
+            # speech_convolved.extend(convolved)
             path='./speech/convolved/'+os.path.basename(file).replace('.wav','_{}.wav'.format(i_loc))
-            # 멀티채널 오디오 파일 저장 찾아봐야 함
-            librosa.output.write_wav(path,)
+            # pdb.set_trace()
+            sf.write(path, convolved, fs)
+            print(path)
 
 
 #32 x 48k x 72
