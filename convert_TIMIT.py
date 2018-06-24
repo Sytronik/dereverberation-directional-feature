062e2f533c64f92f7e6957ce6d2f5f539080e68b
Sytronik <sytronik210@gmail.com>
2018-06-24 23:23:25 +0900

    Changed N3 file name / Refactor

--- convert_TIMIT.py
+++ convert_TIMIT.py
@@ -2,19 +2,12 @@ import os
 from glob import glob
 import soundfile as sf
 
-for dir,_,_ in os.walk('./speech/data/lisa/data/timit/raw/TIMIT/TRAIN/'):
-    files=glob(os.path.join(dir,"*.wav"))
-    if files==[]:
-        continue
-    # for file in files:
-    #     if file.endswith("_converted.wav"):
-    #         os.remove(file)
-
-    for file in files:
-        # print(file)
-        if file.endswith('_converted.wav'):
-            continue
-        data, fs=sf.read(file)
-        newfilename=file.replace('.WAV','_converted.wav')
-        sf.write(newfilename, data, fs)
-        print(newfilename)
+path='./speech/data/lisa/data/timit/raw/TIMIT/TRAIN/'
+for dir,_,_ in os.walk(path):
+    files=glob(os.path.join(dir, '*.wav'))
+    if files is not None:
+        for file in [f for f in files if not f.endswith('_converted.wav')]:
+            data, fs=sf.read(file)
+            newfilename=file.replace('.WAV', '_converted.wav')
+            sf.write(newfilename, data, fs)
+            print(newfilename)
