4df68ca6a6eb1a61d58b69a8c813bb4ad2014b12
Sytronik <sytronik210@gmail.com>
2018-08-03 10:47:36 +0900

    intensity image matlab으로 그리기

--- 1_MATLABCode/N4_analysis_IntensityVect_Result.m
+++ 1_MATLABCode/N4_analysis_IntensityVect_Result.m
@@ -1,30 +1,83 @@
 clear;
 
-load 'IV_RGBA_noreg.mat'
+load '0001_00.mat'
+
+scaling = max(max(max(abs(IV_free(:,:,1:3)))));
+gamma = 50
+IV_free(:,:,1:3) = (IV_free(:,:,1:3) + scaling) ./ (2*scaling);
+IV_free(:,:,1:3) = 1./(1+exp(-gamma*(IV_free(:,:,1:3)-0.5)));
+IV_free(:,:,4) = 10*log10(IV_free(:,:,4)+1e-3);
+[N_freq, x_free] = size(IV_free(:,:,1));
+y = [0 8000];
+x_free = [1 x_free];
+scaling = max(max(max(abs(IV_room(:,:,1:3)))));
+IV_room(:,:,1:3) = (IV_room(:,:,1:3) + scaling) ./ (2*scaling);
+IV_room(:,:,1:3) = 1./(1+exp(-gamma*(IV_room(:,:,1:3)-0.5)));
+IV_room(:,:,4) = 10*log10(IV_room(:,:,4)+1e-3);
+[~, x_room] = size(IV_room(:,:,1));
+x_room = [1 x_room];
+x = max([x_free x_room]);
 
 %% Plot
-hf1 = figure(1);clf;
-imagesc(flipud(permute(IV_free(:,:,1:3)/10,[2 1 3])));
-xlabel('time');
-ylabel('frequency');
-title('Free-field Intensity vector')
+figure(1);clf;
+ax=subplot(2,2,1);
+image(x_free, y, IV_free(:,:,1:3));
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
+title('Free-field Intensity Vector')
 
-hf4 = figure(4);clf;
-imagesc(flipud(permute(20*log10(IV_free(:,:,4)),[2 1 3])));
-xlabel('time');
-ylabel('frequency');
+% hf4 = figure(4);clf;
+ax=subplot(2,2,3);
+image(x_free, y, IV_free(:,:,4),'CDataMapping','scaled');
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
 colorbar
-title('Free-field a00')
+title('Free-field |a_{00}|^2 (dB)')
 
-hf2 = figure(2);clf;
-imagesc(flipud(permute(IV_room(:,:,1:3),[2 1 3])));
-xlabel('time');
-ylabel('frequency');
-title('Room Intensity vector')
+% hf2 = figure(2);clf;
+ax=subplot(2,2,2);
+image(x_room, y, IV_room(:,:,1:3));
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
+title('Room Intensity Vector')
 
-hf3 = figure(3);clf;
-imagesc(flipud(permute(20*log10(IV_room(:,:,4)),[2 1 3])));
-xlabel('time');
-ylabel('frequency');
+% hf3 = figure(3);clf;
+ax=subplot(2,2,4);
+image(x_room, y, IV_room(:,:,4), 'CDataMapping','scaled');
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
 colorbar
-title('Room a00')
+title('Room |a_{00}|^2 (dB)')
+
+n=3;
+figure(2);clf;
+ax=subplot(2,1,1);
+board = repmat((checkerboard(n, floor(N_freq/2/n), floor(x/2/n))>0.5)*0.3+0.7, [1 1 3]);
+image([1 x], y, board);
+hold on;
+image(x_free, y, IV_free(:,:,1:3), 'AlphaData', IV_free(:,:,4),'AlphaDataMapping','scaled');
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
+title('Free-field Intensity Vector + |a_{00}|^2');
+hold off;
+
+ax=subplot(2,1,2);
+image([1 x], y, board);
+hold on;
+image(x_room, y, IV_room(:,:,1:3), 'AlphaData', IV_room(:,:,4),'AlphaDataMapping','scaled');
+ax.YDir = 'normal';
+xlim([1 x]);
+xlabel('frame index');
+ylabel('frequency (Hz)');
+title('Room Intensity Vector + |a_{00}|^2')
+hold off;
