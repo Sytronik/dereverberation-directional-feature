f9f41ed596704503d488bc87044ab8b64287fda6
Sytronik <sytronik210@gmail.com>
2018-08-07 09:25:55 +0900

    plot loss and snr

--- 1_MATLABCode/plot_loss.m
+++ 1_MATLABCode/plot_loss.m
@@ -1,19 +1,22 @@
-load('MLP_loss_49.mat')
+% final_epoch = 60
+% load(['MLP_loss_' num2str(final_epoch) '.mat'])
+% load(['MLP_result_' num2str(final_epoch) '.mat'])
+% clear 'IV_free' 'IV_room' 'IV_estimated'
 
-epochs = 1:length(loss_train);
+epochs = 1:min(find(loss_train==0))-1;
 
 figure(1); clf;
 subplot(2,1,1);
-plot(epochs, loss_train, epochs, loss_valid)
+plot(epochs, loss_train(epochs), epochs, loss_valid(epochs))
 grid on
 xlabel('epoch');
-xlim([epochs(1) epochs(end)]);
-ylabel('MSE loss');
+xlim([0 epochs(end)]);
+ylabel('Squared error loss');
 legend('train set', 'validation set');
 
 subplot(2,1,2);
-plot(epochs, snr_valid_dB);
+plot(epochs, snr_valid_dB(epochs));
 grid on
 xlabel('epoch');
-xlim([epochs(1) epochs(end)]);
+xlim([0 epochs(end)]);
 ylabel('SNR (dB)');
