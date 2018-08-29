6e410062cea957a2781fded7e3bc5008d7832705
Sytronik <sytronik210@gmail.com>
2018-08-29 21:05:35 +0900

    RIR 위치 옮김

--- 1_MATLABCode/RIR_Data/RIR_combine.m
+++
@@ -1,9 +0,0 @@
-azi_ax = [0:5:495];
-RIR = zeros(32,16000,length(azi_ax));
-for ii = 1:length(azi_ax)
-    file_name = ['Anm_FIR_6order_Rand_' num2str(azi_ax(ii)) 'azi_']
-    vaRR = load(file_name);
-    RIR(:,:,ii) = vaRR.h1;
-end
-
-save('RIR.mat','RIR');
