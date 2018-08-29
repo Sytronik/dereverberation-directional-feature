e646c6ba689a698f011db7abfbfcf6fd5b9742fa
Sytronik <sytronik210@gmail.com>
2018-08-29 21:01:05 +0900

    ISO 표준으로 계산

--- 1_MATLABCode/RT60_from_RIR.m
+++ 1_MATLABCode/RT60_from_RIR.m
@@ -1,31 +1,29 @@
 Fs = 16000;
 
-[N_mic, L_RIR, N_loc] = size(RIR);
-cum_reverse = cumsum(RIR(:,end:-1:1,:).^2, 2);
-cum_dB = 10*log10(cum_reverse(:,end:-1:1,:));
-thr_start = cum_dB(:,1,:) - 5;
+[N_mic, L_RIR] = size(RIR);
+cum_reverse = cumsum(RIR(:,end:-1:1).^2, 2);
+cum_dB = 10*log10(cum_reverse(:,end:-1:1));
+thr_start = cum_dB(:,1) - 5;
 thr_end = thr_start - 20;
 
-idx_start = zeros(N_mic, N_loc);
-idx_end = zeros(N_mic, N_loc);
+idx_start = zeros(N_mic, 1);
+idx_end = zeros(N_mic, 1);
 
 for i_mic = 1:N_mic
-    for i_loc = 1:N_loc
-        idx_start(i_mic, i_loc) = find(cum_dB(i_mic, :, i_loc)<thr_start(i_mic, :, i_loc), 1);
-        idx_end(i_mic, i_loc) = find(cum_dB(i_mic, :, i_loc)<thr_end(i_mic, :, i_loc), 1);
-    end
+    idx_start(i_mic) = find(cum_dB(i_mic, :)<thr_start(i_mic, :), 1);
+    idx_end(i_mic) = find(cum_dB(i_mic, :)<thr_end(i_mic, :), 1);
 end
 
 RT20 = (idx_end - idx_start) ./ Fs;
 RT60 = RT20*3;
-RT60_mean = mean(RT60);
+RT60_mean = mean(RT60)
 
 figure(1); clf;
-% stem(RT60.');
-stem(RT60_mean);
+stem(RT60.');
+% stem(RT60_mean);
 xlabel('Source Position Index');
 ylabel('RT60 (sec)');
-xlim([0 N_loc+1]);
+xlim([0 N_mic+1]);
 
 title('Average RT60 for 32ch microphone array');
 
@@ -37,6 +35,7 @@ grid;
 
 fig = gcf;
 set(fig,'renderer','painter');
-% fname = 'RT60_72Azi';
+% fname = 'RT60_Rand_TEST';
 % print('-dpng' , '-r300' , fname)
 % saveas(fig,fname,'fig')
+% close all;
