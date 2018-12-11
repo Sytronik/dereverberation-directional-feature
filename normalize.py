7e0caf9ccf0eae461f6e5bf20d33dc79de516b61
Sytronik <sytronik210@gmail.com>
2018-12-12 01:40:53 +0900

    Refactor Add: EPS Change: Min/Max-> -0.5~0.5

--- normalize.py
+++ normalize.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 import multiprocessing as mp
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Sequence
+from itertools import islice
 
 import deepdish as dd
 
@@ -11,29 +12,29 @@ import torch
 import generic as gen
 from generic import TensArr
 
+EPS = 1e-4
+USE_ONLY_X = False
+
 
 class NormalizationBase(dd.util.Saveable, metaclass=ABCMeta):
-    def __init__(self, **kwargs):
+    __slots__ = 'constnames', 'consts', '__cpu', '__cuda'
+
+    def __init__(self, constnames: Sequence[str], consts: Sequence):
         super().__init__()
-        for k, a in kwargs.items():
-            self.__dict__[k] = a
+        self.constnames = constnames
+        if consts[0].dtype == np.float64:
+            consts = list([a.astype(np.float32) for a in consts])
 
-        self._LEN = len(kwargs)
-
+        self.consts = consts if type(consts) == list else list(consts)
         self.__cpu = None
         self.__cuda = None
 
-    def _convert(self, astype: type, device=torch.device('cpu')):
-        if astype == torch.Tensor:
-            members = {k: gen.convert(a, astype).to(device=device)
-                       for k, a in self.save_to_dict().items()}
-        else:
-            members = {k: gen.convert(a, astype)
-                       for k, a in self.save_to_dict().items()}
-        return type(self)(**members)
+    @staticmethod
+    def _tensor(consts, device=torch.device('cpu')):
+        return [torch.tensor(a, device=device, dtype=torch.float32) for a in consts]
 
     @staticmethod
-    def calc_per_file(tup: Tuple) -> Tuple:
+    def _calc_per_file(tup: Tuple) -> Tuple:
         fname, xname, yname, list_func, args_x, args_y = tup
         try:
             data_dict = dd.io.load(fname)
@@ -46,54 +47,52 @@ class NormalizationBase(dd.util.Saveable, metaclass=ABCMeta):
         print('.', end='', flush=True)
         return result_x, result_y
 
-    def _get_consts_like(self, a: TensArr):
+    def _get_consts_like(self, a: TensArr, xy: str):
+        assert xy == 'x' or xy == 'y'
+        shft = int(xy == 'y') if not USE_ONLY_X else 0
+
+        ch = range(3, 4) if a.shape[-1] == 1 else range(0, 4)
+
         if type(a) == torch.Tensor:
             if a.device == torch.device('cpu'):
                 if not self.__cpu:
-                    self.__cpu = self._convert(type(a), device=a.device)
-                return self.__cpu
+                    self.__cpu = self._tensor(self.consts, device=a.device)
+                result = self.__cpu
             else:
                 if not self.__cuda:
-                    self.__cuda = self._convert(type(a), device=a.device)
-                return self.__cuda
+                    self.__cuda = self._tensor(self.consts, device=a.device)
+                result = self.__cuda
         else:
-            return self
+            result = self.consts
+
+        return [c[..., ch] if c.shape else c for c in islice(result, shft, None, 2)]
 
     def save_to_dict(self) -> Dict:
-        return {k: a for k, a in self.__dict__.items()
-                if not k.startswith('_')}
+        # return {k: a for k, a in self.__dict__.items()
+        #         if not k.startswith('_')}
+        return {s: getattr(self, s) for s in self.__slots__
+                if hasattr(self, s) and not s.startswith('_')}
 
     @classmethod
     def load_from_dict(cls, d: Dict):
         return cls(**d)
 
     def __len__(self):
-        return self._LEN
+        return len(self.consts)
 
     def __str__(self):
         result = ''
-        visited = {str}
-        for k in self.__dict__:
-            name = k[:-2]
-            if k.startswith('_') or name in visited:
-                continue
+        for idx, name in enumerate(self.constnames):
+            x = self.consts[2*idx]
+            y = self.consts[2*idx + 1]
 
             result += f'{name}: '
-            x = self.__dict__[f'{name}_x']
-            y = self.__dict__[f'{name}_y']
             if hasattr(x, 'shape') and x.shape:
                 result += f'{x.shape}, {y.shape}\t'
             else:
                 result += f'{x}, {y}\t'
-            visited.add(name)
 
-        if result.endswith('\t'):
-            result = result[:-1]
-        return result
-
-    @abstractmethod
-    def __getitem__(self, xy: str) -> Tuple:
-        pass
+        return result[:-1]
 
     @abstractmethod
     def normalize(self, a: TensArr, xy: str) -> TensArr:
@@ -113,48 +112,48 @@ class NormalizationBase(dd.util.Saveable, metaclass=ABCMeta):
 
 
 class MeanStdNormalization(NormalizationBase):
-    def __init__(self, mean_x, mean_y, std_x, std_y):
-        super().__init__(
-            mean_x=mean_x,
-            mean_y=mean_y,
-            std_x=std_x,
-            std_y=std_y
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     @classmethod
     def sum_(cls, a: np.ndarray, *args) -> np.ndarray:
         return a.sum(axis=1, keepdims=True)
 
     @classmethod
-    def sq_dev(cls, a: np.ndarray, mean_a)-> np.ndarray:
+    def sq_dev(cls, a: np.ndarray, mean_a) -> np.ndarray:
         return ((a - mean_a)**2).sum(axis=1, keepdims=True)
 
     @classmethod
-    def create(cls, all_files: List[str], xname: str, yname: str):
+    def create(cls, all_files: List[str], xname: str, yname: str, *, need_mean=True):
         # Calculate summation & size (parallel)
+        list_fn = (np.size, cls.sum_) if need_mean else (np.size,)
+        args_none = (None,)*len(list_fn)
         pool = mp.Pool(mp.cpu_count())
         result = pool.map(
-            cls.calc_per_file,
-            [(fname, xname, yname, (np.size,), (None,), (None,))
+            cls._calc_per_file,
+            [(fname, xname, yname, list_fn, args_none, args_none)
              for fname in all_files]
         )
         print()
 
-        # sum_x = np.sum([item[0]['sum'] for item in result], axis=0)
-        # sum_y = np.sum([item[1]['sum'] for item in result], axis=0)
         sum_size_x = np.sum([item[0][np.size] for item in result])
         sum_size_y = np.sum([item[1][np.size] for item in result])
-
-        # mean_x = sum_x[..., :3]/(sum_size_x // sum_x[..., :3].size)
-        # mean_y = sum_y[..., :3]/(sum_size_y // sum_y[..., :3].size)
-        mean_x = 0.
-        mean_y = 0.
-        print('Calculated Mean')
+        if need_mean:
+            sum_x = np.sum([item[0][cls.sum_] for item in result], axis=0)
+            sum_y = np.sum([item[1][cls.sum_] for item in result], axis=0)
+            mean_x = sum_x/(sum_size_x//sum_x.size)
+            mean_y = sum_y/(sum_size_y//sum_y.size)
+            # mean_x = sum_x[..., :3] / (sum_size_x//sum_x[..., :3].size)
+            # mean_y = sum_y[..., :3] / (sum_size_y//sum_y[..., :3].size)
+        else:
+            mean_x = 0.
+            mean_y = 0.
+        print('Calculated Size/Mean')
 
         # Calculate squared deviation (parallel)
         result = pool.map(
-            cls.calc_sq_dev,
-            [(fname, xname, yname, (cls.sq_dev_log,), (mean_x,), (mean_y,))
+            cls._calc_per_file,
+            [(fname, xname, yname, (cls.sq_dev,), (mean_x,), (mean_y,))
              for fname in all_files]
         )
         pool.close()
@@ -166,109 +165,91 @@ class MeanStdNormalization(NormalizationBase):
         std_y = np.sqrt(sum_sq_dev_y/(sum_size_y//sum_sq_dev_y.size) + 1e-5)
         print('Calculated Std')
 
-        return cls(mean_x, mean_y, std_x, std_y)
+        return cls(('mean', 'std'), (mean_x, mean_y, std_x, std_y))
 
     def normalize(self, a: TensArr, xy: str) -> TensArr:
-        mean, std = self._get_consts_like(a)[xy]
-        b = gen.copy(a)
-        # b[..., :3] -= mean
-        b /= std
+        mean, std = self._get_consts_like(a, xy)
+
         return (a - mean)/std
 
     def normalize_(self, a: TensArr, xy: str) -> TensArr:
-        mean, std = self._get_consts_like(a)[xy]
-        # a[..., :3] -= mean
+        mean, std = self._get_consts_like(a, xy)
         a -= mean
         a /= std
         return a
 
     def denormalize(self, a: TensArr, xy: str) -> TensArr:
-        mean, std = self._get_consts_like(a)[xy]
-        b = a*std
-        # b[..., :3] += mean
-        b += mean
-        return b
+        mean, std = self._get_consts_like(a, xy)
+
+        return a*std + mean
 
     def denormalize_(self, a: TensArr, xy: str) -> TensArr:
-        mean, std = self._get_consts_like(a)[xy]
+        mean, std = self._get_consts_like(a, xy)
+
         a *= std
-        # a[..., :3] += mean
         a += mean
         return a
 
-    def __getitem__(self, xy: str) -> Tuple:
-        try:
-            mean = self.__dict__[f'mean_x']
-            std = self.__dict__[f'std_x']
-        except AttributeError:
-            raise Exception('the index should be "x" or "y"')
-
-        return mean, std
-
 
 class MinMaxNormalization(NormalizationBase):
-    def __init__(self, min_x, min_y, max_x, max_y):
-        super().__init__(
-            min_x=min_x,
-            min_y=min_y,
-            max_x=max_x,
-            max_y=max_y
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def min_(cls, a: np.ndarray, *args) -> np.ndarray:
+        return a.min(axis=1, keepdims=True)
+
+    @classmethod
+    def max_(cls, a: np.ndarray, *args) -> np.ndarray:
+        return a.max(axis=1, keepdims=True)
 
     @classmethod
     def create(cls, all_files: List[str], xname: str, yname: str):
         # Calculate summation & no. of total frames (parallel)
         pool = mp.Pool(mp.cpu_count())
         result = pool.map(
-            cls.calc_minmax,
-            [(fname, xname, yname, (np.min, np.max), (None,)*2, (None,)*2)
+            cls._calc_per_file,
+            [(fname, xname, yname, (cls.min_, cls.max_), (None,)*2, (None,)*2)
              for fname in all_files]
         )
         pool.close()
 
-        min_x = np.min([res[0][np.min] for res in result])
-        min_y = np.min([res[1][np.min] for res in result])
-        max_x = np.max([res[0][np.max] for res in result])
-        max_y = np.max([res[1][np.max] for res in result])
+        min_x = np.min([res[0][cls.min_] for res in result], axis=0)
+        min_y = np.min([res[1][cls.min_] for res in result], axis=0)
+        max_x = np.max([res[0][cls.max_] for res in result], axis=0)
+        max_y = np.max([res[1][cls.max_] for res in result], axis=0)
+        print('Calculated Min/Max.')
 
-        return cls(min_x, min_y, max_x, max_y)
+        return cls(('min', 'max'), (min_x, min_y, max_x, max_y))
 
     def normalize(self, a: TensArr, xy: str) -> TensArr:
-        min_, max_ = self._get_consts_like(a)[xy]
-        # return (a - mean)/std - min_
-        return (a - min_)/(max_ - min_)
+        min_, max_ = self._get_consts_like(a, xy)
+
+        return (a - min_)/(max_ - min_) - 0.5
 
     def normalize_(self, a: TensArr, xy: str) -> TensArr:
-        min_, max_ = self._get_consts_like(a)[xy]
-        # return (a - mean)/std - min_
+        min_, max_ = self._get_consts_like(a, xy)
+
         a -= min_
         a /= (max_ - min_)
+        a -= 0.5
         return a
 
     def denormalize(self, a: TensArr, xy: str) -> TensArr:
-        min_, max_ = self._get_consts_like(a)[xy]
-        # return (a + min_)*std + mean
-        return a*(max_ - min_) + min_
+        min_, max_ = self._get_consts_like(a, xy)
+
+        return (a + 0.5)*(max_ - min_) + min_
 
     def denormalize_(self, a: TensArr, xy: str) -> TensArr:
-        min_, max_ = self._get_consts_like(a)[xy]
-        # a += min_
+        min_, max_ = self._get_consts_like(a, xy)
+
+        a += 0.5
         a *= (max_ - min_)
         a += min_
         return a
 
-    def __getitem__(self, xy: str) -> Tuple:
-        try:
-            min_ = self.__dict__[f'min_{xy}']
-            max_ = self.__dict__[f'max_{xy}']
-        except AttributeError:
-            raise Exception('the index should be "x" or "y"')
 
-        return min_, max_
-
-
-class LogMeanStdNormalization(MeanStdNormalization):
-    EPS = 1
+class LogInterface:
     signs = {
         (torch, torch.device('cpu')):
             (torch.tensor((1.,), dtype=torch.float32),
@@ -279,7 +260,7 @@ class LogMeanStdNormalization(MeanStdNormalization):
     }
 
     @classmethod
-    def sign_like(cls, a: TensArr) -> TensArr:
+    def sign_like(cls, a: TensArr) -> Tuple[TensArr, TensArr]:
         if type(a) == torch.Tensor:
             if (torch, a.device) not in cls.signs:
                 cls.signs[(torch, a.device)] = (
@@ -295,10 +276,13 @@ class LogMeanStdNormalization(MeanStdNormalization):
         pkg = gen.dict_package[type(a)]
         plus, minus = cls.sign_like(a)
 
-        b = pkg.zeros_like(a)
-        b[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
-                      * pkg.log10((pkg.abs(a[..., :3]) + cls.EPS)/cls.EPS))
-        b[..., 3] = pkg.log10((a[..., 3] + cls.EPS)/cls.EPS)
+        b = pkg.empty_like(a)
+        if a.shape[-1] == 4:
+            b[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
+                          *pkg.log10((pkg.abs(a[..., :3]) + EPS)/EPS))
+            b[..., 3] = pkg.log10((a[..., 3] + EPS)/EPS)
+        else:
+            b = pkg.log10((a + EPS)/EPS)
         return b
 
     @classmethod
@@ -306,9 +290,12 @@ class LogMeanStdNormalization(MeanStdNormalization):
         pkg = gen.dict_package[type(a)]
         plus, minus = cls.sign_like(a)
 
-        a[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
-                      * pkg.log10((pkg.abs(a[..., :3]) + cls.EPS)/cls.EPS))
-        a[..., 3] = pkg.log10((a[..., 3] + cls.EPS)/cls.EPS)
+        if a.shape[-1] == 4:
+            a[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
+                          *pkg.log10((pkg.abs(a[..., :3]) + EPS)/EPS))
+            a[..., 3] = pkg.log10((a[..., 3] + EPS)/EPS)
+        else:
+            a = pkg.log10((a + EPS)/EPS)
         return a
 
     @classmethod
@@ -316,10 +303,13 @@ class LogMeanStdNormalization(MeanStdNormalization):
         pkg = gen.dict_package[type(a)]
         plus, minus = cls.sign_like(a)
 
-        b = pkg.zeros_like(a)
-        b[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
-                      * cls.EPS*(10.**pkg.abs(a[..., :3]) - 1))
-        b[..., 3] = cls.EPS*(10.**a[..., 3] - 1)
+        b = pkg.empty_like(a)
+        if a.shape[-1] == 4:
+            b[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
+                          *EPS*(10.**pkg.abs(a[..., :3]) - 1))
+            b[..., 3] = EPS*(10.**a[..., 3] - 1)
+        else:
+            b = EPS*(10.**a - 1)
         return b
 
     @classmethod
@@ -327,18 +317,49 @@ class LogMeanStdNormalization(MeanStdNormalization):
         pkg = gen.dict_package[type(a)]
         plus, minus = cls.sign_like(a)
 
-        a[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
-                      * cls.EPS*(10.**pkg.abs(a[..., :3]) - 1))
-        a[..., 3] = cls.EPS*(10.**a[..., 3] - 1)
+        if a.shape[-1] == 4:
+            a[..., :3] = (pkg.where(a[..., :3] > 0, plus, minus)
+                          *EPS*(10.**pkg.abs(a[..., :3]) - 1))
+            a[..., 3] = EPS*(10.**a[..., 3] - 1)
+        else:
+            a = EPS*(10.**a - 1)
         return a
 
+
+class LogMeanStdNormalization(MeanStdNormalization, LogInterface):
     @classmethod
     def sum_(cls, a: np.ndarray, *args) -> np.ndarray:
         return super().sum_(cls.log_(a))
 
     @classmethod
     def sq_dev(cls, a: np.ndarray, mean_a) -> np.ndarray:
-        return super().sum_(cls.log_(a), mean_a)
+        return super().sq_dev(cls.log_(a), mean_a)
+
+    def normalize(self, a: TensArr, xy: str) -> TensArr:
+        b = self.log(a)
+        return super().normalize_(b, xy)
+
+    def normalize_(self, a: TensArr, xy: str) -> TensArr:
+        a = self.log_(a)
+        return super().normalize_(a, xy)
+
+    def denormalize(self, a: TensArr, xy: str) -> TensArr:
+        b = super().denormalize(a, xy)
+        return self.exp_(b)
+
+    def denormalize_(self, a: TensArr, xy: str) -> TensArr:
+        a = super().denormalize_(a, xy)
+        return self.exp_(a)
+
+
+class LogMinMaxNormalization(MinMaxNormalization, LogInterface):
+    @classmethod
+    def min_(cls, a: np.ndarray, *args) -> np.ndarray:
+        return super().min_(cls.log_(a))
+
+    @classmethod
+    def max_(cls, a: np.ndarray, *args) -> np.ndarray:
+        return super().max_(cls.log_(a))
 
     def normalize(self, a: TensArr, xy: str) -> TensArr:
         b = self.log(a)
