fcf3a6a3408dc3f338e933911f267be77bfefbff
Sytronik <sytronik210@gmail.com>
2019-01-17 14:06:41 +0900

    add reevaluation.py, add CustomWriter

---
+++ reevaluation.py
@@ -0,0 +1,71 @@
+# noinspection PyUnresolvedReferences
+import matlab.engine
+
+import os
+import time
+from argparse import ArgumentError, ArgumentParser
+from glob import glob
+
+import scipy.io as scio
+
+import config as cfg
+from train import CustomWriter
+from utils import arr2str
+
+cfg.N_GRIFFIN_LIM = 10
+TITLE = f'iter{cfg.N_GRIFFIN_LIM}'
+
+parser = ArgumentParser()
+parser.add_argument(
+    '--test', action='store_true',
+)
+parser.add_argument(
+    '--valid', action='store_true',
+)
+ARGS = parser.parse_args()
+if ARGS.test:
+    if ARGS.valid:
+        raise ArgumentError
+    else:
+        group = 'test'
+else:
+    if ARGS.valid:
+        group = 'valid'
+    else:
+        group = 'test'
+
+DIR_MODEL = './result/UNet 19-01-14 (amp fix)'
+DIR_ORIGINAL = os.path.join(DIR_MODEL, group)
+DIR_NEW = os.path.join(DIR_MODEL, f'{group}_{TITLE}')
+if not os.path.isdir(DIR_NEW):
+    os.makedirs(DIR_NEW)
+writer = CustomWriter(DIR_NEW)
+
+iv_files = glob(os.path.join(DIR_ORIGINAL, 'IV_*.mat'))
+names = {k: v[1:] for k, v in cfg.IV_DATA_NAME.items() if k != 'fname_wav'}
+avg_measure = None
+
+print(' 0.00%:')
+for idx, file in enumerate(iv_files):
+    t_start = time.time()
+    iv_dict = scio.loadmat(file)
+    iv_dict = {k: iv_dict[v] for k, v in names.items()}
+
+    measure = writer.write_one(idx, group=group, **iv_dict)
+    if avg_measure is None:
+        avg_measure = measure
+    else:
+        avg_measure += measure
+
+    str_measure = arr2str(measure).replace('\n', '; ')
+    tt = time.strftime('(%Ss)', time.gmtime(time.time() - t_start))
+    print(f'{(idx + 1) / len(iv_files) * 100:5.2f}%: {str_measure}\t{tt}')
+
+    avg_measure /= len(iv_files)
+
+writer.add_text(f'{group}/Average Measure/Proposed', str(avg_measure[0]))
+writer.add_text(f'{group}/Average Measure/Reverberant', str(avg_measure[1]))
+
+print()
+str_avg_measure = arr2str(avg_measure).replace('\n', '; ')
+print(f'Average: {str_avg_measure}')
--- train.py
+++ train.py
@@ -11,16 +11,145 @@ from torch.utils.data import DataLoader
 
 import config as cfg
 from adamwr import AdamW, CosineLRWithRestarts
-from audio_utils import (bnkr_equalize_,
-                         delta,
-                         draw_spectrogram,
-                         Measurement,
-                         reconstruct_wave,
-                         )
+from audio_utils import *
 from iv_dataset import IVDataset
 from models import UNet
 from normalize import LogInterface as LogModule
 from utils import (arr2str, MultipleOptimizer, MultipleScheduler, print_progress)
+
+
+class CustomWriter(SummaryWriter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        atexit.register(self.close)
+        self.one_sample = dict()
+        self.recon_sample = dict()
+        self.measure_x = dict()
+        self.kwargs_fig = dict()
+
+    def write_one(self, step: int, out: np.ndarray = None, group='', **kwargs) -> np.ndarray:
+        """ write summary about one sample of output(and x and y optionally).
+
+        :param step:
+        :param out:
+        :param group:
+        :param kwargs: dict(x, y, x_phase, y_phase)
+        :return:
+        """
+
+        assert out is not None
+        if kwargs:
+            one_sample = kwargs
+            do_reuse = False
+        else:
+            one_sample = self.one_sample
+            do_reuse = True if self.recon_sample else False
+
+        if do_reuse:
+            y = self.recon_sample['y']
+            x_phase = self.recon_sample['x_phase']
+            y_phase = self.recon_sample['y_phase']
+            y_wav = self.recon_sample['y_wav']
+            pad_one = self.recon_sample['pad_one']
+
+            snrseg_x = self.measure_x['SNRseg']
+            odict_eval_x = self.measure_x['odict_eval']
+        else:
+            # F, T, 1
+            x = one_sample['x'][..., -1:]
+            y = one_sample['y'][..., -1:]
+
+            x = np.sqrt(x)
+            y = np.sqrt(y)
+
+            x, x_phase = bnkr_equalize_(x, one_sample['x_phase'][:])
+            y, y_phase = bnkr_equalize_(y, one_sample['y_phase'][:])
+
+            snrseg_x = calc_snrseg(y, x[:, :y.shape[1], :])
+
+            # T,
+            x_wav = reconstruct_wave(x, x_phase)
+            y_wav = reconstruct_wave(y, y_phase)
+
+            odict_eval_x = calc_using_eval_module(y_wav, x_wav[:y_wav.shape[0]])
+
+            pad_one = np.ones(
+                (y.shape[0], x.shape[1] - y.shape[1], y.shape[2])
+            )
+            vmin, vmax = 20 * LogModule.log(np.array((y.min(), y.max())))
+
+            fig_x = draw_spectrogram(x)
+            fig_y = draw_spectrogram(np.append(y, y.min() * pad_one, axis=1))
+
+            y_wav = wave_scale_fix(y_wav, message='y_wav')
+            x_wav = wave_scale_fix(x_wav, message='x_wav')
+
+            self.add_figure(f'{group}/1. Anechoic Spectrum', fig_y, step)
+            self.add_figure(f'{group}/2. Reverberant Spectrum', fig_x, step)
+
+            self.add_audio(
+                f'{group}/1. Anechoic Wave', torch.from_numpy(y_wav), step,
+                sample_rate=cfg.Fs
+            )
+            self.add_audio(
+                f'{group}/2. Reverberant Wave', torch.from_numpy(x_wav), step,
+                sample_rate=cfg.Fs
+            )
+
+            self.recon_sample = dict(x=x, y=y,
+                                     x_phase=x_phase, y_phase=y_phase,
+                                     x_wav=x_wav, y_wav=y_wav,
+                                     pad_one=pad_one)
+            self.measure_x = dict(SNRseg=snrseg_x, odict_eval=odict_eval_x)
+            self.kwargs_fig = dict(vmin=vmin, vmax=vmax)
+
+        out = out[..., -1:]
+        np.maximum(out, 0, out=out)
+        np.sqrt(out, out=out)
+        out = bnkr_equalize_(out)
+
+        snrseg = calc_snrseg(y, out)
+
+        out_wav = reconstruct_wave(out, x_phase[:, :out.shape[1], :],
+                                   n_iter=cfg.N_GRIFFIN_LIM)
+        out_wav_y_ph = reconstruct_wave(out, y_phase)
+
+        odict_eval = calc_using_eval_module(y_wav, out_wav)
+
+        out_wav = torch.from_numpy(
+            wave_scale_fix(out_wav, message='out_wav')
+        )
+        out_wav_y_ph = torch.from_numpy(
+            wave_scale_fix(out_wav_y_ph, message='out_wav_y_ph')
+        )
+
+        fig_out = draw_spectrogram(np.append(out, y.min() * pad_one, axis=1),
+                                   **self.kwargs_fig)
+
+        self.add_scalars(f'{group}/1. SNRseg',
+                         dict(Reverberant=snrseg_x,
+                              Proposed=snrseg),
+                         step)
+        # for idx, key in enumerate(odict_eval.keys()):
+        for idx, key in zip([2, 0, 1], odict_eval.keys()):
+            self.add_scalars(f'{group}/{2 + idx}. {key}',
+                             dict(Reverberant=odict_eval_x[key],
+                                  Proposed=odict_eval[key]),
+                             step)
+
+        self.add_figure(
+            f'{group}/3. Estimated Anechoic Spectrum', fig_out, step
+        )
+        self.add_audio(
+            f'{group}/3. Estimated Anechoic Wave', out_wav, step,
+            sample_rate=cfg.Fs
+        )
+        self.add_audio(
+            f'{group}/4. Estimated Wave with Anechoic Phase', out_wav_y_ph, step,
+            sample_rate=cfg.Fs
+        )
+        return np.array([[snrseg]+list(odict_eval.values()),
+                         [snrseg_x]+list(odict_eval_x.values())])
 
 
 class Trainer:
@@ -43,11 +172,7 @@ class Trainer:
 
         self.criterion = cfg.criterion
 
-        self.writer: SummaryWriter = None
-        self.one_sample = dict()
-        self.recon_sample = dict()
-        self.measure_x = dict()
-        self.kwargs_fig = dict()
+        self.writer: CustomWriter = None
 
     def pre(self, x: np.ndarray, y: np.ndarray,
             dataset: IVDataset) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -105,6 +230,15 @@ class Trainer:
             ) if param2 else None,
         )
 
+        # Load State Dict
+        if f_state_dict:
+            tup = torch.load(f_state_dict)
+            try:
+                self.model.module.load_state_dict(tup[0])
+                optimizer.load_state_dict(tup[1])
+            except:
+                raise Exception('The model is different from the state dict.')
+
         # Learning Rates Scheduler
         scheduler = MultipleScheduler(CosineLRWithRestarts,
                                       optimizer,
@@ -113,15 +247,9 @@ class Trainer:
                                       last_epoch=first_epoch - 1,
                                       **cfg.hp.CosineLRWithRestarts)
 
-        # Load State Dict
-        if f_state_dict:
-            tup = torch.load(f_state_dict)
-            self.model.module.load_state_dict(tup[0])
-            optimizer.load_state_dict(tup[1])
         avg_loss = torch.zeros(1, device=cfg.OUT_CUDA_DEV)
 
-        self.writer = SummaryWriter(dir_result, purge_step=first_epoch)
-        atexit.register(self.writer.close)
+        self.writer = CustomWriter(dir_result, purge_step=first_epoch)
 
         # Start Training
         for epoch in range(first_epoch, cfg.hp.n_epochs):
@@ -142,14 +270,13 @@ class Trainer:
 
                 loss = self.calc_loss(y_cuda, output, T_ys)
                 avg_loss += loss.item()
-                # writer.add_histogram()
 
                 # ===================backward====================
                 optimizer.zero_grad()
                 loss.backward()
-                if epoch == 0:
+                if epoch <= 1:
                     gradnorm = nn.utils.clip_grad_norm_(self.model.parameters(), 10**5)
-                    self.writer.add_scalar('grad', gradnorm, epoch*len(loader_train)+i_iter)
+                    self.writer.add_scalar('train/grad', gradnorm, epoch*len(loader_train)+i_iter)
                     del gradnorm
 
                 optimizer.step()
@@ -160,7 +287,7 @@ class Trainer:
                 print_progress(i_iter + 1, len(loader_train), f'epoch {epoch:3d}: {loss:.1e}')
 
             avg_loss /= len(loader_train.dataset)
-            self.writer.add_scalar('train/loss', avg_loss, epoch)
+            self.writer.add_scalar('loss/train', avg_loss, epoch)
 
             # ==================Validation=========================
             self.validate(loader_valid, f_prefix, epoch)
@@ -181,123 +308,6 @@ class Trainer:
         # Test and print
         # loss_test = validate(model, loader=loader_test, )
         # print(f'\nTest Loss: {arr2str(loss_test, n_decimal=4)}')
-
-    def write_one(self, idx: int, out: np.ndarray, *,
-                  group='validation', **kwargs) -> np.ndarray:
-        """ write summary about one sample of output(and x and y optionally).
-
-        :param idx:
-        :param out:
-        :param group:
-        :param kwargs: dict(x, y, x_phase, y_phase)
-        :return:
-        """
-
-        if kwargs:
-            one_sample = kwargs
-            do_reuse = False
-        else:
-            one_sample = self.one_sample
-            do_reuse = True if self.recon_sample else False
-
-        if do_reuse:
-            y = self.recon_sample['y']
-            x_phase = self.recon_sample['x_phase']
-            y_phase = self.recon_sample['y_phase']
-            y_wav = self.recon_sample['y_wav']
-            pad_one = self.recon_sample['pad_one']
-
-            snrseg_x = self.measure_x['SNRseg']
-            pesq_x = self.measure_x['PESQ']
-            stoi_x = self.measure_x['STOI']
-        else:
-            # F, T, 1
-            x = one_sample['x'][..., -1:]
-            y = one_sample['y'][..., -1:]
-
-            np.sqrt(x, out=x)
-            np.sqrt(y, out=y)
-
-            x, x_phase = bnkr_equalize_(x, one_sample['x_phase'])
-            y, y_phase = bnkr_equalize_(y, one_sample['y_phase'])
-
-            snrseg_x = Measurement.calc_snrseg(y, x[:, :y.shape[1], :])
-
-            # T,
-            x_wav = reconstruct_wave(x, x_phase)
-            y_wav = reconstruct_wave(y, y_phase)
-
-            pesq_x, stoi_x = Measurement.calc_pesq_stoi(y_wav, x_wav[:y_wav.shape[0]])
-
-            pad_one = np.ones(
-                (y.shape[0], x.shape[1] - y.shape[1], y.shape[2])
-            )
-            vmin, vmax = 20 * LogModule.log(np.array((y.min(), y.max())))
-
-            fig_x = draw_spectrogram(x)
-            fig_y = draw_spectrogram(np.append(y, y.min() * pad_one, axis=1))
-
-            self.writer.add_figure(f'{group}/1. Anechoic Spectrum', fig_y, idx)
-            self.writer.add_figure(f'{group}/2. Reverberant Spectrum', fig_x, idx)
-
-            self.writer.add_audio(
-                f'{group}/1. Anechoic Wave', torch.from_numpy(y_wav), idx,
-                sample_rate=cfg.Fs
-            )
-            self.writer.add_audio(
-                f'{group}/2. Reverberant Wave', torch.from_numpy(x_wav), idx,
-                sample_rate=cfg.Fs
-            )
-
-            self.recon_sample = dict(x=x, y=y,
-                                     x_phase=x_phase, y_phase=y_phase,
-                                     x_wav=x_wav, y_wav=y_wav,
-                                     pad_one=pad_one)
-            self.measure_x = dict(SNRseg=snrseg_x, PESQ=pesq_x, STOI=stoi_x)
-            self.kwargs_fig = dict(vmin=vmin, vmax=vmax)
-
-        out = out[..., -1:]
-        np.sqrt(out, out=out)
-        out = bnkr_equalize_(out)
-
-        snrseg = Measurement.calc_snrseg(y, out)
-
-        np.maximum(out, 0, out=out)
-
-        out_wav = reconstruct_wave(out, x_phase[:, :out.shape[1], :],
-                                   do_griffin_lim=True)
-        out_wav_y_ph = reconstruct_wave(out, y_phase)
-
-        pesq, stoi = Measurement.calc_pesq_stoi(y_wav, out_wav)
-
-        out_wav = torch.from_numpy(out_wav)
-        out_wav_y_ph = torch.from_numpy(out_wav_y_ph)
-
-        fig_out = draw_spectrogram(np.append(out, y.min() * pad_one, axis=1),
-                                   **self.kwargs_fig)
-
-        self.writer.add_scalars(f'{group}/1. SNRseg',
-                                dict(Reverberant=snrseg_x, Proposed=snrseg),
-                                idx)
-        self.writer.add_scalars(f'{group}/2. PESQ',
-                                dict(Reverberant=pesq_x, Proposed=pesq),
-                                idx)
-        self.writer.add_scalars(f'{group}/3. STOI',
-                                dict(Reverberant=stoi_x, Proposed=stoi),
-                                idx)
-
-        self.writer.add_figure(
-            f'{group}/3. Estimated Anechoic Spectrum', fig_out, idx
-        )
-        self.writer.add_audio(
-            f'{group}/3. Estimated Anechoic Wave', out_wav, idx,
-            sample_rate=cfg.Fs
-        )
-        self.writer.add_audio(
-            f'{group}/4. Estimated Wave with Anechoic Phase', out_wav_y_ph, idx,
-            sample_rate=cfg.Fs
-        )
-        return np.array(((snrseg, pesq, stoi), (snrseg_x, pesq_x, stoi_x)))
 
     def validate(self, loader: DataLoader, f_prefix: str, epoch: int):
         """ Evaluate the performance of the model.
@@ -334,15 +344,15 @@ class Trainer:
                 # ======================write summary==========================
                 if not wrote:
                     # F, T, C
-                    if not self.one_sample:
-                        self.one_sample = IVDataset.decollate_padded(data, 0)
+                    if not self.writer.one_sample:
+                        self.writer.one_sample = IVDataset.decollate_padded(data, 0)
 
                     out_one = output[0, :, :, :T_ys[0]].permute(1, 2, 0)
                     out_one = loader.dataset.denormalize_(out_one, 'y')
                     out_one = out_one.cpu().numpy()
 
-                    IVDataset.save_IV(f'{f_prefix}IV_{epoch}',
-                                      **self.one_sample, out=out_one)
+                    IVDataset.save_iv(f'{f_prefix}IV_{epoch}',
+                                      **self.writer.one_sample, out=out_one)
 
                     wrote = True
 
@@ -350,11 +360,10 @@ class Trainer:
                     #     target=write_one,
                     #     args=(x_one, y_one, x_ph_one, y_ph_one, out_one, epoch)
                     # ).start()
-                    self.write_one(epoch, out_one)
+                    self.writer.write_one(epoch, out_one, group='train')
 
             avg_loss /= len(loader.dataset)
-            if self.writer:
-                self.writer.add_scalar('validation/loss', avg_loss.item(), epoch)
+            self.writer.add_scalar('loss/validation', avg_loss.item(), epoch)
 
             self.model.train()
 
@@ -362,18 +371,17 @@ class Trainer:
 
     def test(self, loader: DataLoader, dir_result: str, f_state_dict: str):
         self.model.module.load_state_dict(torch.load(f_state_dict)[0])
-        self.writer = SummaryWriter(dir_result)
+        self.writer = CustomWriter(dir_result)
         group = os.path.basename(dir_result)
-        atexit.register(self.writer.close)
 
         with torch.no_grad():
             self.model.eval()
 
-            avg_measure = np.zeros((2, 3))
-            t_start = time.time()
+            avg_measure = None
 
-            print('0%:')
+            print(' 0.00%:')
             for i_iter, data in enumerate(loader):
+                t_start = time.time()
                 # =======================get data============================
                 x, y = data['x'], data['y']  # B, F, T, C
                 T_ys = data['T_ys']
@@ -391,30 +399,30 @@ class Trainer:
                 out_one = loader.dataset.denormalize_(out_one, 'y')
                 out_one = out_one.cpu().numpy()
 
-                IVDataset.save_IV(os.path.join(dir_result, f'IV_{i_iter}'),
+                IVDataset.save_iv(os.path.join(dir_result, f'IV_{i_iter}'),
                                   **one_sample, out=out_one)
 
                 # Process(
                 #     target=write_one,
                 #     args=(x_one, y_one, x_ph_one, y_ph_one, out_one, epoch)
                 # ).start()
-                measure = self.write_one(i_iter, out_one, group=group, **one_sample)
-                avg_measure += measure
+                measure = self.writer.write_one(i_iter, out_one, group=group, **one_sample)
+                if avg_measure is None:
+                    avg_measure = measure
+                else:
+                    avg_measure += measure
 
-                str_measure = arr2str(measure).replace('\t', '\n')
-                print(f'{np.round((i_iter + 1)/len(loader)*100, 1)}: {str_measure}')
+                str_measure = arr2str(measure).replace('\n', '; ')
+                tt = time.strftime('(%Ss)', time.gmtime(time.time() - t_start))
+                print(f'{(i_iter + 1)/len(loader)*100:5.2f}%: {str_measure}\t{tt}')
 
             self.model.train()
 
         avg_measure /= len(loader.dataset)
 
-        self.writer.add_text(f'{group}/1. Average SNRseg/Proposed', str(avg_measure[0, 0]))
-        self.writer.add_text(f'{group}/1. Average SNRseg/Reverberant', str(avg_measure[1, 0]))
-        self.writer.add_text(f'{group}/2. Average PESQ/Proposed', str(avg_measure[0, 1]))
-        self.writer.add_text(f'{group}/2. Average PESQ/Reverberant', str(avg_measure[1, 1]))
-        self.writer.add_text(f'{group}/3. Average STOI/Proposed', str(avg_measure[0, 2]))
-        self.writer.add_text(f'{group}/3. Average STOI/Reverberant', str(avg_measure[1, 2]))
+        self.writer.add_text(f'{group}/Average Measure/Proposed', str(avg_measure[0]))
+        self.writer.add_text(f'{group}/Average Measure/Reverberant', str(avg_measure[1]))
 
-        print(time.strftime('%H h %M m', time.gmtime(time.time() - t_start)))
         print()
-        print(arr2str(avg_measure).replace('\t', '\n'))
+        str_avg_measure = arr2str(avg_measure).replace('\n', '; ')
+        print(f'Average: {str_avg_measure}')
