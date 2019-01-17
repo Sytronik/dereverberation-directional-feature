797aa9b48c670bf3f49f20780ab4b86991f63023
Sytronik <sytronik210@gmail.com>
2019-01-17 14:06:26 +0900

    remove Measurement class

--- audio_utils.py
+++ audio_utils.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict as ODict
-from typing import Sequence, Tuple, Union
+from typing import Sequence
 
 import librosa
 import numpy as np
@@ -9,10 +9,9 @@ from scipy.linalg import toeplitz
 
 import config as cfg
 import generic as gen
-from matlab_lib import PESQ_STOI
+from matlab_lib import Evaluation
 from normalize import LogInterface as LogModule
-from pystoi.stoi import stoi
-from utils import arr2str
+from utils import static_vars
 
 
 # class SNRseg(nn.Module):
@@ -37,148 +36,138 @@ from utils import arr2str
 #         sum_result *= 10
 #         return sum_result
 
+# class Measurement:
+#     __slots__ = ('__len',
+#                  '__METRICS',
+#                  '__sum_values',
+#                  )
+#
+#     DICT_CALC = dict(SNRseg='Measurement.calc_snrseg',
+#                      # STOI=Measurement.calc_stoi,
+#                      PESQ_STOI='Measurement.calc_pesq_stoi',
+#                      )
+#
+#     def __init__(self, *metrics):
+#         self.__len = 0
+#         self.__METRICS = metrics
+#         # self.__seq_values = {metric: torch.empty(max_size) for metric in self.__METRICS}
+#         self.__sum_values: ODict = None
+#
+#     def __len__(self):
+#         return self.__len
+#
+#     def __str__(self):
+#         return self._str_measure(self.average())
+#
+#     # def __getitem__(self, idx):
+#     #     return [self.__seq_values[metric][idx] for metric in self.__METRICS]
+#
+#     def average(self):
+#         """
+#
+#         :rtype: OrderedDict[str, torch.Tensor]
+#         """
+#         return ODict([(metric, sum_ / self.__len)
+#                       for metric, sum_ in self.__sum_values.items()])
+#
+#     def append(self, y: np.ndarray, out: np.ndarray,
+#                T_ys: Union[int, Sequence[int]]) -> str:
+#         values = ODict([(metric, eval(self.DICT_CALC[metric])(y, out, T_ys))
+#                         for metric in self.__METRICS])
+#         if self.__len:
+#             for metric, v in values.items():
+#                 # self.__seq_values[metric][self.__len] = self.DICT_CALC[metric](y, out, T_ys)
+#                 self.__sum_values[metric] += v
+#         else:
+#             self.__sum_values = values
+#
+#         self.__len += len(T_ys) if hasattr(T_ys, '__len__') else 1
+#         return self._str_measure(values)
+#
+#     @staticmethod
+#     def _str_measure(values: ODict) -> str:
+#         return '\t'.join(
+#             [f"{metric}={arr2str(v, 'f')} " for metric, v in values.items()]
+#         )
+#
+#
+# def calc_stoi(y_clean: np.ndarray, y_est: np.ndarray):
+#     sum_result = 0.
+#     for item_clean, item_est in zip(y_clean, y_est):
+#         sum_result += stoi(item_clean, item_est, cfg.Fs)
+#     return sum_result
 
-class Measurement:
-    __slots__ = ('__len',
-                 '__METRICS',
-                 '__sum_values',
-                 )
 
-    DICT_CALC = dict(SNRseg='Measurement.calc_snrseg',
-                     # STOI=Measurement.calc_stoi,
-                     PESQ_STOI='Measurement.calc_pesq_stoi',
-                     )
+def calc_snrseg(y_clean: np.ndarray, y_est: np.ndarray,
+                T_ys: Sequence[int] = (0,)) -> np.ndarray:
+    _LIM_UPPER = 35. / 10.  # clip at 35 dB
+    _LIM_LOWER = -10. / 10.  # clip at -10 dB
+    if len(T_ys) == 1 and y_clean.shape[0] != 1:
+        T_ys = (y_clean.shape[0],)
+        y_clean = y_clean[np.newaxis, ...]
+        y_est = y_est[np.newaxis, ...]
 
-    pesq_stoi_module = None
-
-    @staticmethod
-    def calc_snrseg(y_clean: np.ndarray, y_est: np.ndarray,
-                    T_ys: Sequence[int] = (0,)) -> np.ndarray:
-        LIM_UPPER = 35. / 10.  # clip at 35 dB
-        LIM_LOWER = -10. / 10.  # clip at -10 dB
-        if len(T_ys) == 1 and y_clean.shape[0] != 1:
-            T_ys = (y_clean.shape[0],)
-            y_clean = y_clean[np.newaxis, ...]
-            y_est = y_est[np.newaxis, ...]
-
-        sum_result = 0.
-        for T, item_clean, item_est in zip(T_ys, y_clean, y_est):
-            # T
-            norm_clean = np.einsum(
-                'ftc,ftc->t',
-                *((item_clean[:, :T, :],) * 2)
-            )
-            norm_err = np.einsum(
-                'ftc,ftc->t',
-                *((item_est[:, :T, :] - item_clean[:, :T, :],) * 2)
-            ) + np.finfo(np.float64).eps
-
-            snrseg = np.log10(norm_clean / norm_err + np.finfo(np.float64).eps)
-            np.minimum(snrseg, LIM_UPPER, out=snrseg)
-            np.maximum(snrseg, LIM_LOWER, out=snrseg)
-            sum_result += snrseg.mean()
-        sum_result *= 10
-
-        return sum_result
-
-    @classmethod
-    def calc_pesq_stoi(cls, y_clean: np.ndarray, y_est: np.ndarray,
-                       *args) -> Tuple[np.float64, np.float64]:
-        if not cls.pesq_stoi_module:
-            cls.pesq_stoi_module = PESQ_STOI()
-        if y_clean.ndim == 1:
-            y_clean = y_clean[np.newaxis, ...]
-            y_est = y_est[np.newaxis, ...]
-
-        sum_result = np.zeros(2)
-        for item_clean, item_est in zip(y_clean, y_est):
-            temp = cls.pesq_stoi_module(item_clean, item_est, cfg.Fs)
-            sum_result += np.array([temp['PESQ'], temp['STOI']])
-
-        return sum_result[0], sum_result[1]
-
-    @staticmethod
-    def calc_stoi(y_clean: np.ndarray, y_est: np.ndarray):
-        sum_result = 0.
-        for item_clean, item_est in zip(y_clean, y_est):
-            sum_result += stoi(item_clean, item_est, cfg.Fs)
-        return sum_result
-
-    def __init__(self, *metrics):
-        self.__len = 0
-        self.__METRICS = metrics
-        # self.__seq_values = {metric: torch.empty(max_size) for metric in self.__METRICS}
-        self.__sum_values: ODict = None
-
-    def __len__(self):
-        return self.__len
-
-    def __str__(self):
-        return self._str_measure(self.average())
-
-    # def __getitem__(self, idx):
-    #     return [self.__seq_values[metric][idx] for metric in self.__METRICS]
-
-    def average(self):
-        """
-
-        :rtype: OrderedDict[str, torch.Tensor]
-        """
-        return ODict([(metric, sum_ / self.__len)
-                      for metric, sum_ in self.__sum_values.items()])
-
-    def append(self, y: np.ndarray, out: np.ndarray,
-               T_ys: Union[int, Sequence[int]]) -> str:
-        values = ODict([(metric, eval(self.DICT_CALC[metric])(y, out, T_ys))
-                        for metric in self.__METRICS])
-        if self.__len:
-            for metric, v in values.items():
-                # self.__seq_values[metric][self.__len] = self.DICT_CALC[metric](y, out, T_ys)
-                self.__sum_values[metric] += v
-        else:
-            self.__sum_values = values
-
-        self.__len += len(T_ys) if hasattr(T_ys, '__len__') else 1
-        return self._str_measure(values)
-
-    @staticmethod
-    def _str_measure(values: ODict) -> str:
-        return '\t'.join(
-            [f"{metric}={arr2str(v, 'f')} " for metric, v in values.items()]
+    sum_result = 0.
+    for T, item_clean, item_est in zip(T_ys, y_clean, y_est):
+        # T
+        norm_clean = np.einsum(
+            'ftc,ftc->t',
+            *((item_clean[:, :T, :],) * 2)
         )
+        norm_err = np.einsum(
+            'ftc,ftc->t',
+            *((item_est[:, :T, :] - item_clean[:, :T, :],) * 2)
+        ) + np.finfo(np.float64).eps
 
-    # stft_module = STFT(N_fft, L_hop)
-    #
-    # if USE_CUDA:
-    #     stft_module = nn.DataParallel(stft_module).cuda()
+        snrseg = np.log10(norm_clean / norm_err + np.finfo(np.float64).eps)
+        np.minimum(snrseg, _LIM_UPPER, out=snrseg)
+        np.maximum(snrseg, _LIM_LOWER, out=snrseg)
+        sum_result += snrseg.mean()
+    sum_result *= 10
 
-    # measure_mag = Measurement('SNRseg')
-    # measure_wav = Measurement('PESQ_STOI')
+    return sum_result
 
 
-def reconstruct_wave(mag: np.ndarray, phase: np.ndarray,
-                     *, n_sample=-1, do_griffin_lim=False) -> np.ndarray:
-    mag = mag.squeeze()
-    phase = phase.squeeze()
+@static_vars(module=None)
+def calc_using_eval_module(y_clean: np.ndarray, y_est: np.ndarray,
+                           T_ys: Sequence[int] = (0,)) -> ODict:
+    if not calc_using_eval_module.module:
+        calc_using_eval_module.module = Evaluation()
+    if y_clean.ndim == 1:
+        y_clean = y_clean[np.newaxis, ...]
+        y_est = y_est[np.newaxis, ...]
+    if T_ys == (0,):
+        T_ys = (y_clean.shape[1],)*y_clean.shape[0]
 
-    if do_griffin_lim:
-        wave = griffin_lim(mag, phase, n_sample=n_sample, n_iter=cfg.N_GRIFFIN_LIM)
-    else:
-        wave = librosa.core.istft(mag * np.exp(1j * phase), **cfg.KWARGS_ISTFT)
-        if n_sample != -1:
-            wave = wave[:n_sample]
+    keys = None
+    sum_result = None
+    for T, item_clean, item_est in zip(T_ys, y_clean, y_est):
+        temp = calc_using_eval_module.module(item_clean[:T], item_est[:T], cfg.Fs)
+        result = np.array(list(temp.values()))
+        if not keys:
+            keys = temp.keys()
+            sum_result = result
+        else:
+            sum_result += result
 
-    max_amplitude = np.max(np.abs(wave))
-    if max_amplitude > 1:
-        wave /= max_amplitude
-        print(f'wave is scaled by {max_amplitude} to prevent clipping.')
+    return ODict(zip(keys, sum_result.tolist()))
+
+
+def wave_scale_fix(wave: np.ndarray, amp_limit=1., message='') -> np.ndarray:
+    max_amp = np.max(np.abs(wave))
+    if max_amp > amp_limit:
+        wave /= max_amp
+        if message:
+            print(f'{message} is scaled by {max_amp / amp_limit} to prevent clipping.')
 
     return wave
 
 
-def griffin_lim(mag: np.ndarray, phase: np.ndarray,
-                *, n_iter: int, n_sample=-1) -> np.ndarray:
-    # wave = scsig.istft(mag*np.exp(1j*phase), **kwargs_stft)[-1]
+def reconstruct_wave(mag: np.ndarray, phase: np.ndarray,
+                     *, n_iter=0, n_sample=-1) -> np.ndarray:
+    mag = mag.squeeze()
+    phase = phase.squeeze()
+
     for _ in range(n_iter - 1):
         wave = librosa.core.istft(mag * np.exp(1j * phase), **cfg.KWARGS_ISTFT)
 
@@ -187,10 +176,7 @@ def griffin_lim(mag: np.ndarray, phase: np.ndarray,
 
     kwarg_len = dict(length=n_sample) if n_sample != -1 else dict()
     wave = librosa.core.istft(mag * np.exp(1j * phase), **cfg.KWARGS_ISTFT, **kwarg_len)
-    # wave = stft_module.inverse(mag, phase)[..., :n_sample]
-    # for _ in range(n_iter - 1):
-    #     _, phase = stft_module.transform(wave)
-    #     wave = stft_module.inverse(mag, phase)[..., :n_sample]
+
     return wave
 
 
@@ -268,9 +254,20 @@ def draw_spectrogram(data: gen.TensArr, is_power=False, show=False, **kwargs):
 
 
 def bnkr_equalize_(mag, phase=None):
-    mag *= cfg.bEQf0_mag
+    assert mag.shape[0] == cfg.bEQf0_mag.shape[0]
+    bEQf0_mag = cfg.bEQf0_mag
+    while mag.ndim < bEQf0_mag.ndim:
+        bEQf0_mag = bEQf0_mag[..., 0]
+
+    mag *= bEQf0_mag
+
     if phase is not None:
-        phase += cfg.bEQf0_angle
+        assert phase.shape[0] == mag.shape[0]
+        bEQf0_angle = cfg.bEQf0_angle
+        while phase.ndim < bEQf0_mag.ndim:
+            bEQf0_angle = bEQf0_angle[..., 0]
+
+        phase += bEQf0_angle
         return mag, phase
     else:
         return mag
