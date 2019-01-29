c3508ab96e18cda587f0bef65ae874029778f366
Sytronik <sytronik210@gmail.com>
2019-01-29 16:10:18 +0900

    bug fix and add file deletion option

--- merge_json.py
+++ merge_json.py
@@ -1,8 +1,8 @@
 import openpyxl
-import csv
 import numpy as np
 import json
 import os
+from os.path import join as pathjoin
 from collections import defaultdict
 from argparse import ArgumentParser
 
@@ -11,6 +11,7 @@ parser = ArgumentParser()
 # parser.add_argument('--title1', nargs=1, type=str, metavar='title1')
 parser.add_argument('path', nargs='?', type=str, const='./')
 parser.add_argument('--tag', nargs=1, type=str, default=('',))
+parser.add_argument('--delete', '-d', action='store_true')
 ARGS = parser.parse_args()
 files = [f.path
          for f in os.scandir(ARGS.path)
@@ -19,17 +20,19 @@ if not files:
     print('No files.')
     exit()
 
-step = None
-measures = dict()
+title = ARGS.tag[0]
 col_title = ''
 
 common = defaultdict(lambda: 0)
+files_chosen = []
 for fname in files:
-    fname = os.path.basename(fname)
-    fname_split = fname.replace('.json', '').split('_')
-    if ARGS.tag[0] not in fname_split:
+    fname_split = os.path.basename(fname)
+    fname_split = fname_split.replace('-tag-', '_')
+    fname_split = fname_split.replace('.json', '').split('_')
+    if title not in fname_split:
         continue
 
+    files_chosen.append(fname)
     print(fname)
 
     if not col_title:
@@ -46,12 +49,15 @@ if not col_title:
 print()
 
 col_names = [k for k, v in common.items() if v == 1]
-title = ARGS.tag[0]
+step = None
+measures = dict()
 
-for fname, col_name in zip(files, col_names):
+for fname, col_name in zip(files_chosen, col_names):
     with open(fname) as f:
         data = json.loads(f.read())
         data = np.array(data)
+    if ARGS.delete:
+        os.remove(fname)
     if not step:  # and ARGS.title1:
         step = data[:, 1].tolist()
     measures[col_name] = data[:, 2]
@@ -75,7 +81,7 @@ for ii, (key, value) in enumerate(measures.items()):
     for jj, (cell,) in enumerate(ws[f'{cols[ii]}3:{cols[ii]}{3 + value.shape[0] - 1}']):
         cell.value = value[jj]
 
-fname_result = os.path.join(ARGS.path, f'merged_{title}_{col_title}.xlsx')
+fname_result = pathjoin(ARGS.path, f'merged_{title}_{col_title}.xlsx')
 while True:
     try:
         wb.save(fname_result)
