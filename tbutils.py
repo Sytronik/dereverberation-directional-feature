6dbdcf13efe2aadbb8c82c60f82e1f68dc06b30b
Sytronik <sytronik210@gmail.com>
2019-02-14 15:10:32 +0900

    bar graph, except reverb, empty title
    <details>
    add draw bar graph
    can except 'reverberant'
    support empty title

--- tbutils.py
+++ tbutils.py
@@ -1,16 +1,17 @@
+import csv
 import json
 import os
 from argparse import ArgumentParser
 from glob import glob
+from itertools import product as iterprod
 from os.path import join as pathjoin
-from typing import List, NamedTuple
-
-import numpy as np
-from tensorboard.backend.event_processing.event_multiplexer import EventMultiplexer
-from tqdm import tqdm
-import csv
+from typing import List, NamedTuple, Sequence, Union
 
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy import ndarray
+from tensorboard.backend.event_processing.event_multiplexer import EventMultiplexer
+from tqdm import tqdm
 
 from mypath import *
 
@@ -34,6 +35,7 @@ need_all_scalars = False if SUFFIX == 'train' else True
 
 Y_MAX = dict(PESQ=4.5, STOI=1., )
 
+NEED_REVERB = False
 
 class IdxSuffixCol(NamedTuple):
     idx: int
@@ -55,6 +57,7 @@ class IdxSuffixCol(NamedTuple):
 
 def save_scalars_from_tfevents(path: str, suffix: str):
     DIR_EVENTS = pathjoin(path, suffix)
+    suffix = suffix.split('_')[0]
 
     dict_logdirs = {}
     for folder, _, _ in os.walk(DIR_EVENTS):
@@ -93,10 +96,104 @@ def save_scalars_from_tfevents(path: str, suffix: str):
         json.dump(dict(step=step_longest, **scalars), f)
 
 
+def draw_bar_graph(titles: Union[str, Sequence[str]],
+                   means: ndarray, stds: ndarray = None, xticklabels: Sequence = None,
+                   ylabel: str = ''):
+    # constants
+    bar_width = 0.5 / len(titles)
+    ndigits = 3 if means.max() - means.min() < 0.1 else 2
+
+    fig, ax = plt.subplots()
+
+    # args
+    if means.ndim == 1:
+        if type(titles) == str:
+            titles = (titles,)
+        means = means[np.newaxis, :]
+        stds = stds[np.newaxis, :] if stds is not None else (None,)
+    if stds is None:
+        stds = (None,) * len(titles)
+
+    common = None
+    if xticklabels:
+        common = {x.split('_')[0] for x in xticklabels}
+        if len(common) == 1:
+            xticklabels = ['_'.join(x.split('_')[1:]) for x in xticklabels]
+        ax.set_xticklabels(xticklabels)
+    if len(titles) == 1:
+        if titles[0] == '.' and common and len(common) == 1:
+            titles = tuple(common)
+            draw_legend = True
+        else:
+            draw_legend = False
+    else:
+        draw_legend = True
+
+    # colors
+    cmap = plt.get_cmap('tab20c')
+    colors = [cmap.colors[(1 + 4*i//16)%4 + (4*i)%16] for i in range(len(titles))]
+    colors[-1] = cmap.colors[17]
+
+    # draw bar & text
+    range_ = np.arange(means.shape[1])
+    for ii, (title, mean, std) in enumerate(zip(titles, means, stds)):
+        bar = ax.bar(range_ + bar_width * ii, mean,
+                     bar_width,
+                     yerr=std,
+                     error_kw=dict(capsize=5),
+                     label=title,
+                     color=colors[ii])
+
+        for b in bar:
+            x = b.get_x() + b.get_width()*0.55
+            y = b.get_height()
+            ax.text(x, y, f'{b.get_height():.{ndigits}f}',
+                    # horizontalalignment='center',
+                    verticalalignment='bottom')
+
+    ax.grid(True, axis='y')
+    ax.set_axisbelow(True)
+
+    # xlim
+    xlim = list(ax.get_xlim())
+    xlim[0] -= bar_width
+    xlim[1] += bar_width
+    ax.set_xlim(*xlim)
+
+    # ylim
+    ylim = list(ax.get_ylim())
+    max_ = (means + stds).max()
+    min_ = (means - stds).min()
+    ylim[0] = min_ - (max_ - min_)*0.2
+    if ylabel in Y_MAX:
+        ylim[1] = min(Y_MAX[ylabel], ylim[1])
+    ax.set_ylim(*ylim)
+
+    # axis label
+    ax.set_xlabel('RIR')
+    ax.set_ylabel(ylabel)
+
+    # ticks
+    ax.set_xticks(range_ + bar_width * (len(titles) - 1) / 2)
+
+    ax.tick_params('x', length=0)
+    ax.tick_params('y', direction='in')
+
+    # legend
+    if draw_legend:
+        ax.legend()
+
+    fig.tight_layout()
+    return fig
+
+
 if __name__ == '__main__':
-    paths = [p for p in glob(pathjoin(PATH_RESULT, PRIMARY)) if os.path.isdir(p)]
-    paths = [p for p in paths if COND_PRIMARY(p)]
-    paths = sorted(paths)
+    if TITLE_PRIMARY:
+        paths = [p for p in glob(pathjoin(PATH_RESULT, PRIMARY)) if os.path.isdir(p)]
+        paths = [p for p in paths if COND_PRIMARY(p)]
+        paths = sorted(paths)
+    else:
+        paths = []
 
     if ARGS.tbcommand:
         BASELINE = pathjoin(BASELINE, SUFFIX)
@@ -131,13 +228,17 @@ if __name__ == '__main__':
         # os.system(f'echo \'{command}\' | pbcopy')
     else:
         if len(paths) > 1:
-            titles = ([TITLE_BASELINE]
-                      + [f'{TITLE_PRIMARY}{idx}' for idx in range(len(paths))]
-                      + ['Reverberant'])
+            titles = (([TITLE_BASELINE] if TITLE_BASELINE else [])
+                      + [f'{TITLE_PRIMARY}{idx}' for idx in range(len(paths))])
         else:
-            titles = [TITLE_BASELINE, TITLE_PRIMARY, 'Reverberant']
+            titles = [TITLE_BASELINE, TITLE_PRIMARY]
+            titles = [t for t in titles if t]
+        if NEED_REVERB:
+            titles += ['reverberant']
+
         length = len(titles)
-        paths.insert(0, pathjoin(PATH_RESULT, BASELINE))
+        if TITLE_BASELINE:
+            paths.insert(0, pathjoin(PATH_RESULT, BASELINE))
         if ARGS.save_scalars:
             for path in tqdm(paths):
                 save_scalars_from_tfevents(path, SUFFIX)
@@ -147,15 +248,28 @@ if __name__ == '__main__':
             data = {}
             suffixes: list = None
             for idx, path in enumerate(paths):
-                paths_suffix, suffixes = zip(*((p.path, p.name) for p in os.scandir(path)
-                                               if p.is_dir() and p.name != 'train'))
+                def cond(p):
+                    return (p.is_dir()
+                            and p.name != 'train'
+                            and p.name.startswith('unseen')  # warning
+                            and os.path.isfile(pathjoin(p.path, 'scalars.json')))
+
+
+                paths_suffix, suffixes = zip(*((p.path, p.name)
+                                               for p in os.scandir(path)
+                                               if cond(p)))
+                paths_suffix = sorted(paths_suffix)
+                suffixes = sorted(suffixes)
                 assert suffixes, f'{path} is empty.'
                 # suffixes = [os.path.basename(p) for p in paths_suffix]
                 for suffix, p in zip(suffixes, paths_suffix):
-                    with open(pathjoin(p, 'scalars.json'), 'r') as f:
-                        temp = json.loads(f.read())
-                        for k, v in temp.items():
-                            data[IdxSuffixCol(idx, suffix, k)] = v
+                    try:
+                        with open(pathjoin(p, 'scalars.json'), 'r') as f:
+                            temp = json.loads(f.read())
+                            for k, v in temp.items():
+                                data[IdxSuffixCol(idx, suffix, k)] = v
+                    except:
+                        pass
 
             # arrange data
             data_arranged = {}
@@ -169,26 +283,26 @@ if __name__ == '__main__':
                     col_new = key.col.split('/')[0].split('. ')[-1]
                     cols.add(col_new)
                     if 'Reverberant' in key:
-                        key_rev = IdxSuffixCol(-1, key.suffix, col_new)
-                        if key_rev not in data_arranged:
-                            data_arranged[key_rev] = value
+                        if NEED_REVERB:
+                            key_rev = IdxSuffixCol(-1, key.suffix.split('_')[0], col_new)
+                            if key_rev not in data_arranged:
+                                data_arranged[key_rev] = value
                     else:
                         data_arranged[key.mutated_ver(_col=col_new)] = value
             data = data_arranged
             cols: list = sorted(cols)
-            idxs = list(range(length-1)) + [-1]
+            idxs = list(range(length))
+            if NEED_REVERB:
+                idxs[-1] = -1
             del data_arranged
 
             # calculate mean, std
             means = {}
             stds = {}
-            upper = {}
-            lower = {}
             for key, value in data.items():
                 means[key] = np.mean(value)
                 stds[key] = np.std(value, ddof=1)
-                upper[key] = means[key] + stds[key]
-                lower[key] = means[key] - stds[key]
+
 
             # save data, mean and std to csv
             def make_rows(stats):
@@ -203,133 +317,36 @@ if __name__ == '__main__':
                         rows[idx] += [stats[IdxSuffixCol(i, s, col)] for i in idxs]
                 return rows
 
-            fmerged = ', '.join([f'{os.path.basename(p)} ({t})'
-                                 for p, t in zip(paths, titles[:-1])])
-            fmerged = pathjoin(PATH_RESULT, fmerged + '.csv')
+            if len(paths) > 1:
+                fresult = ', '.join([f'{os.path.basename(p)} ({t})'
+                                     for p, t in zip(paths, titles[:-1] if NEED_REVERB else titles)])
+            else:
+                fresult = f'{os.path.basename(paths[0])} ' + ', '.join(['_'.join(s.split('_')[1:]) for s in suffixes])
+            fmerged = pathjoin(PATH_RESULT, fresult + '.csv')
             with open(fmerged, 'w', newline='') as f:
                 writer = csv.writer(f)
                 rows = []
                 for idx, title in zip(idxs, titles):
                     rows.append([title])
-                    for suffix in suffixes:
-                        for ii, col in enumerate(cols):
-                            rows.append([suffix if ii == 0 else '',
-                                         col,
-                                         *data[IdxSuffixCol(idx, suffix, col)],
-                                         ]
-                                        )
+                    for suffix, (ii, col) in iterprod(suffixes, enumerate(cols)):
+                        rows.append([suffix if ii == 0 else '',
+                                     col,
+                                     *data[IdxSuffixCol(idx, suffix, col)],
+                                     ]
+                                    )
                 rows += [['means'], *make_rows(means)]
                 rows += [['stds'], *make_rows(stds)]
                 for r in rows:
                     writer.writerow(r)
 
-            # plot data
+            # draw bar graph
+            fbar = pathjoin(PATH_FIG, fresult + ' ({}).png')
+            for col in cols:
+                mean = np.empty((length, len(suffixes)))
+                std = np.empty((length, len(suffixes)))
+                for (i, idx), (j, suffix) in iterprod(enumerate(idxs), enumerate(suffixes)):
+                    mean[i, j] = means[IdxSuffixCol(idx, suffix, col)]
+                    std[i, j] = stds[IdxSuffixCol(idx, suffix, col)]
 
-            # suffixes = sorted(list({k.suffix for k in means}))
-            # indices = sorted(list({k.idx for k in means}))
-            # titles_sorted = [titles[idx] for idx in indices]
-            # length = len(titles_sorted)
-            # cols = sorted(list({k.col for k in means}))
-            # # SuffIdxs = [(suffix, idx) for suffix in suffixes for idx in indices]
-            # # x = [(suffix, title) for suffix in suffixes for title in titles_sorted]
-            # for col in cols:
-            #     # top = [means[IdxSuffixCol(i, s, col)] for s, i in SuffIdxs]
-            #     # data = {'suffixes': suffixes}
-            #     data = {titles[i]: [means[IdxSuffixCol(i, s, col)]
-            #                         for s in suffixes]
-            #             for i in indices}
-            #     data_upper = {titles[i]: [upper[IdxSuffixCol(i, s, col)]
-            #                               for s in suffixes]
-            #                   for i in indices}
-            #     data_lower = {titles[i]: [lower[IdxSuffixCol(i, s, col)]
-            #                               for s in suffixes]
-            #                   for i in indices}
-            #     data['suffixes'] = suffixes
-            #     source = ColumnDataSource(data=data)
-            #     plt.output_file(pathjoin(PATH_FIG, f'{col}.html'))
-            #     p = plt.figure(plot_height=350,
-            #                    x_range=suffixes,
-            #                    x_axis_label='RIR',
-            #                    y_axis_label=col,
-            #                    toolbar_location=None, tools=''
-            #                    )
-            #     max_ = max(sum(zip(*list(data.values())[:-1]), ()))
-            #     min_ = min(sum(zip(*list(data.values())[:-1]), ()))
-            #     if max_ - min_ < 1:
-            #         ndigits = 3
-            #     else:
-            #         ndigits = 2
-            #     for idx, title in enumerate(titles_sorted):
-            #         x_dodged = dodge(
-            #             'suffixes',
-            #             1 / (length + 1) * (idx - (length - 1) / 2),
-            #             range=p.x_range
-            #         )
-            #         p.vbar(source=source,
-            #                x=x_dodged, top=title,
-            #                width=0.8 / (length + 1),
-            #                color=Palette[length][idx],
-            #                legend=bkcp.value(title),
-            #                )
-            #
-            #         # text
-            #         text = [f'{round(d, ndigits):.{ndigits}f}'
-            #                 for d in data[title]
-            #                 ]
-            #         label_source = ColumnDataSource(
-            #             data=dict(x=suffixes, y=data[title], text=text)
-            #         )
-            #         x_text_dodged = dodge(
-            #             'x',
-            #             1 / (length + 1) * (idx - (length - 1) / 2),
-            #             range=p.x_range
-            #         )
-            #         labels = LabelSet(source=label_source,
-            #                           x=x_text_dodged, y='y', text='text',
-            #                           level='glyph',
-            #                           x_offset=15,
-            #                           y_offset=2 if data[title][0] > 0 else -40,
-            #                           angle=45,
-            #                           render_mode='canvas',
-            #                           )
-            #         p.add_layout(labels)
-            #
-            #         # error bar
-            #         whisker_source = ColumnDataSource(
-            #             data=dict(base=suffixes,
-            #                       upper=data_upper[title],
-            #                       lower=data_lower[title],
-            #                       )
-            #         )
-            #         base_dodged = dodge(
-            #             'base',
-            #             1 / (length + 1) * (idx - (length - 1) / 2),
-            #             range=p.x_range
-            #         )
-            #         p.add_layout(
-            #             Whisker(source=whisker_source,
-            #                     base=base_dodged, upper="upper", lower="lower",
-            #                     level="overlay",
-            #                     )
-            #         )
-            #     p.xgrid.grid_line_color = None
-            #     p.x_range.range_padding = 0.1
-            #
-            #     max_ = max(sum(zip(*list(data_upper.values())[:-1]), ()))
-            #     min_ = min(sum(zip(*list(data_lower.values())[:-1]), ()))
-            #
-            #     p.y_range.start = min_
-            #     p.y_range.start -= 0.2 * (max_ - min_) if min_ > 0 else 0.1 * (max_ - min_)
-            #     p.y_range.end = max_ + 0.2 * (max_ - min_)
-            #     if col in Y_MAX:
-            #         p.y_range.end = min(p.y_range.end, Y_MAX[col])
-            #
-            #     # p.legend.orientation = 'horizontal'
-            #     p.legend.location = 'top_right'
-            #     p.xaxis.major_tick_line_color = None
-            #     p.axis.axis_label_text_font_style = 'normal'
-            #     p.xaxis.major_label_text_font_size = '10pt'
-            #     p.legend.label_text_font_size = '9pt'
-            #     p.legend.orientation = 'horizontal'
-            #     plt.show(p)
-            #     bkio.export_png(p, filename=pathjoin(PATH_FIG, f'{col}.png'))
+                figbar = draw_bar_graph(titles, mean, std, suffixes, col)
+                figbar.savefig(fbar.format(col), dpi=300)
