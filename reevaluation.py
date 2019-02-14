704ade824786b53168e888bacbb164bfbc0b2d98
Sytronik <sytronik210@gmail.com>
2019-02-14 15:12:03 +0900

    use tqdm progress bar

--- reevaluation.py
+++ reevaluation.py
@@ -7,7 +7,9 @@ from argparse import ArgumentParser
 from glob import glob
 from os.path import join as pathjoin
 
+import numpy as np
 import scipy.io as scio
+from tqdm import tqdm
 
 import config as cfg
 from train import CustomWriter
@@ -33,15 +35,19 @@ if not os.path.isdir(DIR_NEW):
 writer = CustomWriter(DIR_NEW)
 
 iv_files = glob(pathjoin(DIR_ORIGINAL, 'IV_*.mat'))
-names = {k: v[1:] for k, v in cfg.IV_DATA_NAME.items() if k != 'fname_wav'}
+names = {k: v[1:] for k, v in cfg.IV_DATA_NAME.items()
+         if k != 'fname_wav' and k != 'out_phase'}
 avg_measure = None
 
 
-print(' 0.00%:')
-for idx, file in enumerate(iv_files):
-    t_start = time.time()
+pbar = tqdm(iv_files, desc=f'{group}_{TITLE}', dynamic_ncols=True)
+for idx, file in enumerate(pbar):
     iv_dict = scio.loadmat(file)
     iv_dict = {k: iv_dict[v] for k, v in names.items()}
+
+    # warning sqrt
+    iv_dict = {k: (np.sqrt(np.maximum(v, 0)) if not k.endswith('phase') else v)
+               for k, v in iv_dict.items()}
 
     measure = writer.write_one(idx, group=group,
                                eval_with_y_ph=True,  # info: the objective of reevaluation
@@ -52,8 +58,7 @@ for idx, file in enumerate(iv_files):
         avg_measure += measure
 
     str_measure = arr2str(measure).replace('\n', '; ')
-    tt = time.strftime('(%Ss)', time.gmtime(time.time() - t_start))
-    print(f'{(idx + 1) / len(iv_files) * 100:5.2f}%: {str_measure}\t{tt}')
+    pbar.write(str_measure)
 
 avg_measure /= len(iv_files)
 
